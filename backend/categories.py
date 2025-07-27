7d9ed13ed26ee63273656f9867fd729ffd8f2704
FelicitasKlix <felikpc@gmail>
2025-07-27 19:24:17 -0300

    Initial commit - Rosco

---
+++ backend/categories.py
@@ -0,0 +1,59 @@
+ciencias_exactas = {
+    "Matematica": "Matematica",
+    "Fisica": "Fisica",
+    "Quimica": "Quimica",
+    "Biologia": "Biologia",
+}
+
+argentinidad = {
+    "Famosos": "Famosos",
+    "Comidas": "Comidas",
+    "Musica": "Musica",
+    "Costumbres": "Costumbres",
+    "Fechas": "Fechas",
+    "Proceres": "Proceres",
+    "Frases": "Frases",
+    "Variado": "Variado",
+    "Peliculas": "Peliculas",
+}
+
+ciencias_sociales = {
+    "Historia": "Historia",
+    "Geografia": "Geografia",
+    "Politica": "Politica",
+    "Libros": "Libros",
+    "Autores": "Autores",
+    "Conceptos": "Conceptos",
+    "Corrientes": "Corrientes"
+}
+
+espectaculo = {
+    "Peliculas": "Peliculas",
+    "Series": "Series",
+    "Cine": "Cine",
+    "Teatro": "Teatro",
+}
+
+programacion = {
+    "Programacion": "Programacion",
+    "Lenguajes": "Lenguajes",
+    "Frameworks": "Frameworks",
+    "Bases de Datos": "Bases de Datos",
+    "Algoritmos": "Algoritmos",
+
+}
+
+linguistica = {
+    "Autores": "Autores",
+    "Conceptos": "Conceptos",
+    "Libros": "Libros",
+    "Subdisciplinas": "Subdisciplinas",
+    "Categorias": "Categorias",
+}
+
+the_l_word = {
+    "Personajes": "Personajes",
+    "Parejas": "Parejas",
+    "Profesiones": "Profesiones",
+    "Quien hizo/dijo": "Quien hizo/dijo",
+}
---
+++ backend/questions.py
@@ -0,0 +1,118 @@
+preg1 = {
+    "pregunta": "Moneda utilizada durante el gobiero de Alfonsin",
+    "respuesta": "Australes",
+    "categoria": "argentinidad",
+    "subcategoria": "Variado"
+}
+
+preg2 = {
+    "pregunta": "Empresa de termos nacional",
+    "respuesta": "Lumilagro",
+    "categoria": "argentinidad",
+    "subcategoria": "Variado"
+}
+
+preg3 = {
+    "pregunta": "Autor del himno nacional",
+    "respuesta": "Vicente Lopez y Planes",
+    "categoria": "argentinidad",
+    "subcategoria": ["Musica", "Proceres"]
+}
+
+preg4 = {
+    "pregunta": "Dupla del pop",
+    "respuesta": "Miranda",
+    "categoria": "argentinidad",
+    "subcategoria": "Musica"
+}
+
+preg5 = {
+    "pregunta": "Pelicula en la que actuan Gasatlla, China Zorrilla y Betiana Blum",
+    "respuesta": "Esperando la carroza",
+    "categoria": ["argentinidad", "espectaculo"],
+    "subcategoria": "Peliculas"
+}
+
+preg6 = {
+    "pregunta": "Arbol nacional",
+    "respuesta": "Ceibo",
+    "categoria": "argentinidad",
+    "subcategoria": "Variado"
+}
+
+preg7 = {
+    "pregunta": "Fauna autoctona de Nordelta",
+    "respuesta": "Carpincho",
+    "categoria": "argentinidad",
+    "subcategoria": "Variado"
+}
+
+preg8 = {
+    "pregunta": "Carcel y aeropuerto",
+    "respuesta": "Ezeiza",
+    "categoria": "argentinidad",
+    "subcategoria": "Variado"
+}
+
+preg9 = {
+    "pregunta": "Medio por el que CFK fugo un PBI",
+    "respuesta": "Bolsos",
+    "categoria": "argentinidad",
+    "subcategoria": "Variado"
+}
+
+preg10 = {
+    "pregunta": "Una de las hijas de Maradona",
+    "respuesta": "Dalma",
+    "categoria": "argentinidad",
+    "subcategoria": "Famosos"
+}
+
+preg11 = {
+    "pregunta": "Marca de papel higienico y servilletas",
+    "respuesta": "Felpita",
+    "categoria": "argentinidad",
+    "subcategoria": "Variado"
+}
+
+preg12 = {
+    "pregunta": "Compositora de 'haceme la co'",
+    "respuesta": "Noelia Pompa",
+    "categoria": "argentinidad",
+    "subcategoria": "Famosos"
+}
+
+preg13 = {
+    "pregunta": "Con que cazo el mono liso la naranja?",
+    "respuesta": "Tenedor",
+    "categoria": "argentinidad",
+    "subcategoria": "Musica"
+}
+
+preg14 = {
+    "pregunta": "Comic de radiación",
+    "respuesta": "Eternauta",
+    "categoria": "argentinidad",    
+    "subcategoria": "Peliculas"
+}
+
+preg15 = {
+    "pregunta": "Poema nacional",
+    "respuesta": "Martin Fierro",
+    "categoria": "argentinidad",
+    "subcategoria": "Variado"
+}
+
+preg16 = {
+    "pregunta": "Que pega papel con papel",
+    "respuesta": "Voligoma",
+    "categoria": "argentinidad",
+    "subcategoria": "Variado"
+}
+
+preg17 = {
+    "pregunta": "",
+    "respuesta": "",
+    "categoria": "argentinidad",
+    "subcategoria": "Variado"
+}
